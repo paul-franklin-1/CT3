21ce2113bb8188e589551bfa4b1eea2d401880a4
Paul Franklin <paul.franklin@csuglobal.edu>
2024-01-30 21:41:52 -0500

    rough draft of code is finished

--- CT3.java
+++ CT3.java
@@ -7,15 +7,12 @@ public class CT3 {
         int weeklyIncome = scnr.nextInt();
         if (weeklyIncome < 500) {
             System.out.print("This person has a 10% tax rate.");
-        }else if (weeklyIncome >= 500 && weeklyIncome<1500) {;
+        }else if (weeklyIncome >= 500 && weeklyIncome<1500) {
             System.out.print("This person has a 15% tax rate.");
-        }else if (weeklyIncome>=1500 && weeklyIncome<2500 ) {;
+        }else if (weeklyIncome>=1500 && weeklyIncome<2500 ) {
             System.out.print("This person has a 20% tax rate.");
-        }else (weeklyIncome >= 2500);
+        }else{
             System.out.print("This person has a 30% tax rate.");
-
-
-
         }
 
 }}
