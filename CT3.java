23c4c89de897115017f1cda285b94229315b975b
Paul Franklin <paul.franklin@csuglobal.edu>
2024-01-30 21:10:57 -0500

    adding statements if else else if

--- CT3.java
+++ CT3.java
@@ -8,7 +8,7 @@ public class CT3 {
         if (weeklyIncome < 500){
             System.out.print("This person has a 10% tax witholding rate.")};
             else if (500<weeklyIncome) and (weeklyIncome<1500);{
-                System.out.print(This person has a 15% tax witholding rate.)
+                System.out.print("This person has a 15% tax witholding rate.");}
 
 
 
