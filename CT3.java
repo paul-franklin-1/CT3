9e3faf20d97e04c1cca1b9613e26aa95aa3c5f26
Paul Franklin <paul.franklin@csuglobal.edu>
2024-01-30 21:35:30 -0500

    all statements added

--- CT3.java
+++ CT3.java
@@ -5,11 +5,14 @@ public class CT3 {
         Scanner scnr = new Scanner(System.in);
         System.out.println("Enter weekly income:");
         int weeklyIncome = scnr.nextInt();
-        if (weeklyIncome < 500){
-            System.out.print("This person has a 10% tax witholding rate.")};
-            else if (500<weeklyIncome) and (weeklyIncome<1500);{
-                System.out.print("This person has a 15% tax witholding rate.");}
-        dasdf
+        if (weeklyIncome < 500) {
+            System.out.print("This person has a 10% tax rate.");
+        }else if (weeklyIncome >= 500 && weeklyIncome<1500) {;
+            System.out.print("This person has a 15% tax rate.");
+        }else if (weeklyIncome>=1500 && weeklyIncome<2500 ) {;
+            System.out.print("This person has a 20% tax rate.");
+        }else (weeklyIncome >= 2500);
+            System.out.print("This person has a 30% tax rate.");
 
 
 
