d6cd1bc51349d7a1b756a0f887c32f1d42eae086
Paul Franklin <paul.franklin@csuglobal.edu>
2024-02-05 17:29:01 -0500

    honing rough draft

--- CT4.java
+++ CT4.java
@@ -1,20 +1,42 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+
+import static java.lang.Float.sum;
+import static java.lang.Math.min;
+
 public class CT4 {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
-        List listGPA = new ArrayList();
+        List listInvestments = new ArrayList();
         int i = 0;
-        float gradePointAvg = 1.00f;
+        float g = 0;
+        float newInvestment = 1.0f;
         while(i<=9){
-            System.out.println("Enter GPA " + (i+1) + " with two decimal places" );
-            gradePointAvg = scnr.nextFloat();
-            listGPA.add(gradePointAvg);
+            System.out.println("Enter value of Investment #" + (i+1) + "  with two decimal places: ");
+            newInvestment = scnr.nextFloat();
+            listInvestments.add(newInvestment);
+            g = g + newInvestment;
             i++;
         }
-        System.out.println(listGPA);
+        float countInvestments = listInvestments.size();
+        float sumInvestments = g;
+        float meanInvestments = sumInvestments / countInvestments;
+        Collections.sort(listInvestments);
+        float minInvestment = (float) listInvestments.get(0);
+        float maxInvestment = (float) listInvestments.get(9);
+        float interestInvestment = (float) (sumInvestments * .2);
+
+        System.out.println("Number of investments: " + countInvestments);
+        System.out.println("Sum of investments: " + sumInvestments);
+        System.out.println("Average investment: " + meanInvestments);
+        System.out.println("Minimum investment: " + minInvestment);
+        System.out.println("Maximum investment: " + minInvestment);
+        System.out.println("20% interest from investments: " + interestInvestment);
+        System.out.println("Have a nice day!");
+    }}
 
 
-    }
-}
+
+
