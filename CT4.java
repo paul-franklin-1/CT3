be0e3185114eec34cea06aa9acbc71afa0cdb554
Paul Franklin <paul.franklin@csuglobal.edu>
2024-02-05 16:50:51 -0500

    rough draft complete

--- CT4.java
+++ CT4.java
@@ -1,6 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-public class CT3 {
+public class CT4 {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
-        ffff
+        List listGPA = new ArrayList();
+        int i = 0;
+        float gradePointAvg = 1.00f;
+        while(i<=9){
+            System.out.println("Enter GPA " + (i+1) + " with two decimal places" );
+            gradePointAvg = scnr.nextFloat();
+            listGPA.add(gradePointAvg);
+            i++;
+        }
+        System.out.println(listGPA);
 
+
+    }
+}
