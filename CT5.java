bbb6988d7ba3db773da11b3b68ffd91a43e4d7d5
Paul Franklin <paul.franklin@csuglobal.edu>
2024-02-13 13:16:04 -0500

    working on the mathematics

--- CT5.java
+++ CT5.java
@@ -1,32 +1,51 @@
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.*;
+import java.util.stream.DoubleStream;
 
 public class CT5 {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
-            double[] avgUSATemps = new double[12];
-        avgUSATemps[0] = 35.13;
-        avgUSATemps[1] = 36.52;
-        avgUSATemps[2] = 44.08;
-        avgUSATemps[3] = 50.65;
-        avgUSATemps[4] = 61.88;
-        avgUSATemps[5] = 70.79;
-        avgUSATemps[6] = 76.41;
-        avgUSATemps[7] = 74.59;
-        avgUSATemps[8] = 68.02;
-        avgUSATemps[9] = 55.26;
-        avgUSATemps[10] = 40.89;
-        avgUSATemps[11] = 33.06;
+        double[] monthlyAvgUSA = new double[12];
+        String[] months2022 = new String[12];
+        String userMonth;
 
-            String[] monthsYear = new String[12];
-        monthsYear[0] = "January";
-        monthsYear[1] = "February";
-        monthsYear[2] = "March";
-        monthsYear[3] = "April";
-        monthsYear[4] = "May";
-        monthsYear[5] = "June";
-        monthsYear[6] = "July";
-        monthsYear[7] = "August";
-        monthsYear[8] = "September";
-        monthsYear[9] = "October";
-        monthsYear[10] = "November";
-        monthsYear[11] = "December";
+        monthlyAvgUSA[0] = 35.13;
+        monthlyAvgUSA[1] = 36.52;
+        monthlyAvgUSA[2] = 44.08;
+        monthlyAvgUSA[3] = 50.65;
+        monthlyAvgUSA[4] = 61.88;
+        monthlyAvgUSA[5] = 70.79;
+        monthlyAvgUSA[6] = 76.41;
+        monthlyAvgUSA[7] = 74.59;
+        monthlyAvgUSA[8] = 68.02;
+        monthlyAvgUSA[9] = 55.26;
+        monthlyAvgUSA[10] = 40.89;
+        monthlyAvgUSA[11] = 33.06;
+
+
+        months2022[0] = "January";
+        months2022[1] = "February";
+        months2022[2] = "March";
+        months2022[3] = "April";
+        months2022[4] = "May";
+        months2022[5] = "June";
+        months2022[6] = "July";
+        months2022[7] = "August";
+        months2022[8] = "September";
+        months2022[9] = "October";
+        months2022[10] = "November";
+        months2022[11] = "December";
+
+        DoubleStream stream = DoubleStream.of(monthlyAvgUSA);
+        DoubleStream stream2 = DoubleStream.of(monthlyAvgUSA);
+        DoubleStream stream3 = DoubleStream.of(monthlyAvgUSA);
+        DoubleStream stream4 = DoubleStream.of(monthlyAvgUSA);
+        double sumAvg = stream.sum();
+        double countAvg = stream2.count();
+        double annualAvg = sumAvg / countAvg;
+        double annualau
+        System.out.println(annualAvg);
+        System.out.println("For country USA in year 2022, please enter month to get average nationwide temperature for that month:");
+        userMonth = scnr.nextLine();
+    }}
