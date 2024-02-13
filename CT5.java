6606c0f093b7406b0dd646e6186a6491cb68ad8f
Paul Franklin <paul.franklin@csuglobal.edu>
2024-02-13 12:36:34 -0500

    entered temp and month values into two distinct arrays

--- CT5.java
+++ CT5.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class CT5 {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
-        double[] avgUSATemps = new double[12];
+            double[] avgUSATemps = new double[12];
         avgUSATemps[0] = 35.13;
         avgUSATemps[1] = 36.52;
         avgUSATemps[2] = 44.08;
@@ -17,3 +17,16 @@ public class CT5 {
         avgUSATemps[10] = 40.89;
         avgUSATemps[11] = 33.06;
 
+            String[] monthsYear = new String[12];
+        monthsYear[0] = "January";
+        monthsYear[1] = "February";
+        monthsYear[2] = "March";
+        monthsYear[3] = "April";
+        monthsYear[4] = "May";
+        monthsYear[5] = "June";
+        monthsYear[6] = "July";
+        monthsYear[7] = "August";
+        monthsYear[8] = "September";
+        monthsYear[9] = "October";
+        monthsYear[10] = "November";
+        monthsYear[11] = "December";
