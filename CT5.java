90b2657be39aceaa8bec5ba407b502958da6e8f0
Paul Franklin <paul.franklin@csuglobal.edu>
2024-02-13 12:30:00 -0500

    first statements

---
+++ CT5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CT5 {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        double[] avgUSATemps = new double[12];
+        avgUSATemps[0] = 35.13;
+        avgUSATemps[1] = 36.52;
+        avgUSATemps[2] = 44.08;
+        avgUSATemps[3] = 50.65;
+        avgUSATemps[4] = 61.88;
+        avgUSATemps[5] = 70.79;
+        avgUSATemps[6] = 76.41;
+        avgUSATemps[7] = 74.59;
+        avgUSATemps[8] = 68.02;
+        avgUSATemps[9] = 55.26;
+        avgUSATemps[10] = 40.89;
+        avgUSATemps[11] = 33.06;
+
